55d7214d5d5827e70b4b6d03aa19e014d7457496
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2024-11-11 18:39:16 +0700

    forgot: database config

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+
+function getDatabaseConfig(): array
+{
+    return [
+        "server_name" => "localhost",
+        "database" => "BebasTanggungan",
+        "username" => "",
+        "password" => "",
+    ];
+}
