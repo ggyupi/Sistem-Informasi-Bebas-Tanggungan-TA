0da1c14a1dabe8d4076d7de2be70b03f7a99b1e2
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2024-11-14 08:22:10 +0700

    feat: darkmode change some color

--- app/views/template/footer.php
+++ app/views/template/footer.php
@@ -1,14 +1,10 @@
 <script>
-    document.getElementById('toggle-side-bar-btn').addEventListener('click', function() {
-        var toggleSidebar = document.getElementById('toggle-side-bar');
-        toggleSidebar.checked = !toggleSidebar.checked;
+    function setUiState(uiStateMap) {
         $.ajax({
             type: "POST",
             url: "setUiState",
             data: {
-                set_ui_state: {
-                    'toggle_sidebar': toggleSidebar.checked
-                }
+                set_ui_state: uiStateMap
             },
             success: function() {
                 console.log("Update UI State Success");
@@ -17,14 +13,29 @@
                 console.log("[Update UI State]: " + response);
             }
         });
+    }
+
+    document.getElementById('toggle-side-bar-btn').addEventListener('click', function() {
+        var toggleSidebar = document.getElementById('toggle-side-bar');
+        toggleSidebar.checked = !toggleSidebar.checked;
+        setUiState({
+            'toggle_sidebar': toggleSidebar.checked
+        });
+    });
+
+    document.getElementById('toggle-dark-mode').addEventListener('click', function() {
+        const html = document.getElementById('html');
+        html.setAttribute('data-bs-theme', html.getAttribute('data-bs-theme') === 'dark' ? 'light' : 'dark');
+        setUiState({
+            'theme_mode': html.getAttribute('data-bs-theme')
+        });
     });
 
     const sidebarNav = document.getElementsByClassName('side-bar-nav');
     for (let i = 0; i < sidebarNav.length; i++) {
         for (let j = 0; j < sidebarNav[i].children.length; j++) {
             const button = sidebarNav[i].children[j];
-            const buttonTitle = button.children[2];
-            console.log(buttonTitle.innerHTML);
+            const buttonTitle = button.children[2];      
             if (buttonTitle.innerHTML == '<?= ucwords(isset($data['page']) ? $data['page'] : 'Dashboard') ?>') {
                 button.classList.add('side-bar-btn-selected');
                 break;
--- app/views/template/sidebar/sidebar-nav2.php
+++ app/views/template/sidebar/sidebar-nav2.php
@@ -1,23 +1,23 @@
 <form class="side-bar-nav">
-                <button class="side-bar-btn" type="submit" name="page" value="notifications">
-                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12.4999 14.1667H16.6666L15.4958 12.9959C15.1783 12.6784 14.9999 12.2477 14.9999 11.7987V9.16667C14.9999 6.98964 13.6086 5.13757 11.6666 4.45118V4.16667C11.6666 3.24619 10.9204 2.5 9.99992 2.5C9.07944 2.5 8.33325 3.24619 8.33325 4.16667V4.45118C6.39126 5.13757 4.99992 6.98964 4.99992 9.16667V11.7987C4.99992 12.2477 4.82154 12.6784 4.50403 12.9959L3.33325 14.1667H7.49992M12.4999 14.1667V15C12.4999 16.3807 11.3806 17.5 9.99992 17.5C8.61921 17.5 7.49992 16.3807 7.49992 15V14.1667M12.4999 14.1667H7.49992" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    <span></span>
-                    <p>Notifications</p>
-                </button>
-                <button class="side-bar-btn" type="submit" name="page" value="support">
-                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10.8333 13.3333H10V10H9.16667M10 6.66667H10.0083M17.5 10C17.5 14.1421 14.1421 17.5 10 17.5C5.85786 17.5 2.5 14.1421 2.5 10C2.5 5.85786 5.85786 2.5 10 2.5C14.1421 2.5 17.5 5.85786 17.5 10Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    <span></span>
-                    <p>Support</p>
-                </button>
-                <button class="side-bar-btn" type="submit" name="page" value="dark_mode">
-                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M8.05289 14.1667H11.947M9.99995 2.5V3.33333M15.3033 4.6967L14.714 5.28596M17.5 9.99996H16.6667M3.33333 9.99996H2.5M5.28591 5.28595L4.69665 4.6967M7.05368 12.9463C5.42649 11.3191 5.42649 8.68093 7.05368 7.05374C8.68086 5.42656 11.319 5.42656 12.9462 7.05374C14.5734 8.68093 14.5734 11.3191 12.9462 12.9463L12.4903 13.4022C11.9629 13.9296 11.6666 14.6449 11.6666 15.3908V15.8333C11.6666 16.7538 10.9204 17.5 9.99995 17.5C9.07948 17.5 8.33329 16.7538 8.33329 15.8333V15.3908C8.33329 14.6449 8.037 13.9296 7.50959 13.4022L7.05368 12.9463Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    <span></span>
-                    <p>Dark Mode</p>
-                </button>
-            </form>
+    <button class="side-bar-btn" type="submit" name="page" value="notifications">
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12.4999 14.1667H16.6666L15.4958 12.9959C15.1783 12.6784 14.9999 12.2477 14.9999 11.7987V9.16667C14.9999 6.98964 13.6086 5.13757 11.6666 4.45118V4.16667C11.6666 3.24619 10.9204 2.5 9.99992 2.5C9.07944 2.5 8.33325 3.24619 8.33325 4.16667V4.45118C6.39126 5.13757 4.99992 6.98964 4.99992 9.16667V11.7987C4.99992 12.2477 4.82154 12.6784 4.50403 12.9959L3.33325 14.1667H7.49992M12.4999 14.1667V15C12.4999 16.3807 11.3806 17.5 9.99992 17.5C8.61921 17.5 7.49992 16.3807 7.49992 15V14.1667M12.4999 14.1667H7.49992" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+        <span></span>
+        <p>Notifications</p>
+    </button>
+    <button class="side-bar-btn" type="submit" name="page" value="support">
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.8333 13.3333H10V10H9.16667M10 6.66667H10.0083M17.5 10C17.5 14.1421 14.1421 17.5 10 17.5C5.85786 17.5 2.5 14.1421 2.5 10C2.5 5.85786 5.85786 2.5 10 2.5C14.1421 2.5 17.5 5.85786 17.5 10Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+        <span></span>
+        <p>Support</p>
+    </button>
+    <div class="side-bar-btn" name="page" id="toggle-dark-mode" value="dark_mode">
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8.05289 14.1667H11.947M9.99995 2.5V3.33333M15.3033 4.6967L14.714 5.28596M17.5 9.99996H16.6667M3.33333 9.99996H2.5M5.28591 5.28595L4.69665 4.6967M7.05368 12.9463C5.42649 11.3191 5.42649 8.68093 7.05368 7.05374C8.68086 5.42656 11.319 5.42656 12.9462 7.05374C14.5734 8.68093 14.5734 11.3191 12.9462 12.9463L12.4903 13.4022C11.9629 13.9296 11.6666 14.6449 11.6666 15.3908V15.8333C11.6666 16.7538 10.9204 17.5 9.99995 17.5C9.07948 17.5 8.33329 16.7538 8.33329 15.8333V15.3908C8.33329 14.6449 8.037 13.9296 7.50959 13.4022L7.05368 12.9463Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+        <span></span>
+        <p>Dark Mode</p>
+    </div>
+</form>
