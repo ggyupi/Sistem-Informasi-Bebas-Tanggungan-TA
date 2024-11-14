b7376ccca438e48ffa2469cb24bda869505d23dc
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2024-11-14 07:26:48 +0700

    feat: menu selected

--- app/views/template/footer.php
+++ app/views/template/footer.php
@@ -1,7 +1,7 @@
 <script>
     document.getElementById('toggle-side-bar-btn').addEventListener('click', function() {
         var toggleSidebar = document.getElementById('toggle-side-bar');
-        toggleSidebar.checked = !toggleSidebar.checked;       
+        toggleSidebar.checked = !toggleSidebar.checked;
         $.ajax({
             type: "POST",
             url: "setUiState",
@@ -18,4 +18,17 @@
             }
         });
     });
+
+    const sidebarNav = document.getElementsByClassName('side-bar-nav');
+    for (let i = 0; i < sidebarNav.length; i++) {
+        for (let j = 0; j < sidebarNav[i].children.length; j++) {
+            const button = sidebarNav[i].children[j];
+            const buttonTitle = button.children[2];
+            console.log(buttonTitle.innerHTML);
+            if (buttonTitle.innerHTML == '<?= ucwords(isset($data['page']) ? $data['page'] : 'Dashboard') ?>') {
+                button.classList.add('side-bar-btn-selected');
+                break;
+            }
+        }
+    }
 </script>
