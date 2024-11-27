de3276efe47c6b0820b018195b83b51979b62c12
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2024-11-27 10:23:08 +0700

    update pengumpulan ui

--- app/views/admin/screens/pengumpulan.php
+++ app/views/admin/screens/pengumpulan.php
@@ -12,19 +12,33 @@ include VIEWS . 'component/btn-icon.php';
     }
 </style>
 
+<form id="dialog-acc">
+    <?=
+    dialogYesNo(
+        'btn-acc',
+        'Acc?',
+        'Logout dan Hapus Sesi Saat ini',
+        SvgIcons::getIcon(Icons::Check) . 'Acc kan min',
+        SvgIcons::getIcon(Icons::Close) . 'Ga Jadi',
+        true
+    );
+    ?>
+</form>
+<form id="dialog-decl">
+    <?=
+    dialogYesNo(
+        'btn-decl',
+        'Tolak',
+        'Logout dan Hapus Sesi Saat ini',
+        SvgIcons::getIcon(Icons::Check) . 'Tolak saja min',
+        SvgIcons::getIcon(Icons::Close) . 'Ga Jadi',
+        true
+    );
+    ?>
+</form>
+
 <div id="pengumpulan-page">
-    <form id="dialog-acc">
-        <?=
-        dialogYesNo(
-            'btn-acc',
-            'Acc?',
-            'Logout dan Hapus Sesi Saat ini',
-            SvgIcons::getIcon(Icons::Check) . 'Acc kan min',
-            SvgIcons::getIcon(Icons::Close) . 'Ga Jadi',
-            true
-        );
-        ?>
-    </form>
+
     <div id="page-content-top">
         <h1>Data Dokumen</h1>
         <div id="page-content-top-right">
@@ -45,6 +59,7 @@ include VIEWS . 'component/btn-icon.php';
     </div>
 
     <div id="table-wrapper">
+
         <table class="table table-hover">
             <thead>
                 <tr>
@@ -99,16 +114,25 @@ include VIEWS . 'component/btn-icon.php';
             });
         }
 
+        function changeModalDialogMessage(id, message) {
+            let dialog = document.getElementById(id);
+            dialog.getElementsByClassName('modal-body')[0].innerHTML = message;
+        }
+
         let idTableExpand = -1;
 
         function generateTableBodyItems(data) {
             const iconAcc = '<?= SvgIcons::getIcon(Icons::Check) ?>';
             const iconDecl = '<?= SvgIcons::getIcon(Icons::Close) ?>';
             const iconQuestion = '<?= SvgIcons::getIcon(Icons::Question) ?>';
-            const btnSee = '<?= iconButton('btn-see', Icons::Eye, 'var(--bs-emphasis-color)') ?>';
-            const btnAcc = '<?= iconButton('btn-acc', Icons::Check, 'green') ?>';
-            const btnDecl = '<?= iconButton('btn-decl', Icons::Close, 'red') ?>';
-            const badgeTertanggung = '<?= statusBadge('warning', Icons::Close, 'Tertanggung') ?>';
+            const btnSee = '<?= iconButton('btn-see', Icons::Eye, 'var(--bs-emphasis-color)', '#onclick') ?>';
+            const btnAcc = '<?= iconButton('btn-acc', Icons::Check, 'green', '#onclick') ?>';
+            const btnDecl = '<?= iconButton('btn-decl', Icons::Close, 'red', '#onclick') ?>';
+            const badgeTertanggung = `
+            <div class="d-flex flex-row align-items-center" style="gap: 12px">
+                <div class="status-badge-text">1</div> <?= statusBadge('warning', Icons::Close, 'Tertanggung') ?>
+            </div>
+            `;
             const badgeSelesai = '<?= statusBadge('success', Icons::Check, 'Selesai') ?>';
 
             let tableBody = document.createElement('tbody');
@@ -116,66 +140,80 @@ include VIEWS . 'component/btn-icon.php';
             for (let i = 0; i < data.length; i++) {
                 let tr = document.createElement('tr');
                 let dataMahasiswa = data[i].data_mahasiswa;
+                let dataDetails = data[i].data_detail;
+                let tuntas = true;
+                for (const i of dataDetails) {
+                    if (i.status.toLowerCase() !== 'diverifikasi') {
+                        tuntas = false;
+                        break;
+                    }
+                }
                 tr.innerHTML = `
                     <td>${dataMahasiswa.id}</td>
                     <td>${dataMahasiswa.nim}</td>
                     <td>${dataMahasiswa.nama}</td>
                     <td>${dataMahasiswa.jurusan}</td>
                     <td>${dataMahasiswa.programStudi}</td> 
-                    <td style="place-items: center">${dataMahasiswa.status.toLowerCase() ===
-                     "selesai" ? badgeSelesai : badgeTertanggung}</td>                   
+                    <td style="place-items: center">${tuntas ? badgeSelesai : badgeTertanggung}</td>                   
                 `;
                 if (i == idTableExpand) {
                     tr.classList.add('table-active');
                 }
                 tableBody.appendChild(tr);
 
-                // OnExampand
-                let dataDetail = data[i].data_detail;
+                // OnExampand           
                 tr = document.createElement('tr');
-
                 let td = document.createElement('td');
                 td.colSpan = 6;
                 td.classList.add('table-expand-wrapper');
-
                 let tableExpand = document.createElement('div');
                 tableExpand.classList.add('table-expand');
-                let tableExpandItem = document.createElement('div');
-                tableExpandItem.classList.add('table-expand-item');
+                for (const dataDetail of dataDetails) {
+                    let tableExpandItem = document.createElement('div');
+                    tableExpandItem.classList.add('table-expand-item');
+                    let tableExpandItemDocument = document.createElement('div');
+                    tableExpandItemDocument.classList.add('d-flex', 'flex-row', 'align-items-center', 'action');
+                    tableExpandItemDocument.id = 'table-expand-item';
+                    let tableExpandItemIcon = document.createElement('div');
+                    tableExpandItemIcon.classList.add('status-badge-icon');
+                    let tableExpandItemText = document.createElement('p');
+                    tableExpandItemText.innerText = dataDetail.dokumen;
+                    let tableExpandItemAction = document.createElement('div');
+                    tableExpandItemAction.classList.add('d-flex', 'flex-row', 'align-items-center', 'action');
+                    tableExpandItemAction.id = 'action';
 
-                let tableExpandItemDocument = document.createElement('div');
-                tableExpandItemDocument.classList.add('d-flex', 'flex-row', 'align-items-center', 'action');
-                tableExpandItemDocument.id = 'table-expand-item';
-                let tableExpandItemIcon = document.createElement('div');
-                tableExpandItemIcon.classList.add('status-badge-icon');
-                let tableExpandItemText = document.createElement('p');
-                tableExpandItemText.innerText = dataDetail.dokumen;
-                let tableExpandItemAction = document.createElement('div');
-                tableExpandItemAction.classList.add('d-flex', 'flex-row', 'align-items-center', 'action');
-                tableExpandItemAction.id = 'action';
+                    let actions = [
+                        btnSee,
+                        btnAcc.replace(`#onclick`, `changeModalDialogMessage('dialog-acc', 
+                            'Acc <strong>${dataDetail.dokumen}</strong>?')`),
+                        btnDecl.replace(`#onclick`, `changeModalDialogMessage('dialog-decl', 
+                        'Tolak <strong>${dataDetail.dokumen}</strong>?')`)
+                    ];
+                    console.log(actions[0]);
 
-                let actions = [btnSee, btnAcc, btnDecl];
-                if (dataDetail.status.toLowerCase() == 'diverifikasi') {
-                    tableExpandItemIcon.classList.add('success');
-                    actions.splice(1, 1);
-                    tableExpandItemIcon.innerHTML = iconAcc;
+                    if (dataDetail.status.toLowerCase() == 'diverifikasi') {
+                        tableExpandItemIcon.classList.add('success');
+                        actions.splice(1, 1);
+                        tableExpandItemIcon.innerHTML = iconAcc;
 
-                } else if (dataDetail.status.toLowerCase() == 'menunggu') {
-                    tableExpandItemIcon.classList.add('warning');
-                    tableExpandItemIcon.innerHTML = iconQuestion;
-                } else {
-                    tableExpandItemIcon.classList.add('danger');
-                    tableExpandItemIcon.innerHTML = iconDecl;
+                    } else if (dataDetail.status.toLowerCase() == 'menunggu') {
+                        tableExpandItemIcon.classList.add('warning');
+                        tableExpandItemIcon.innerHTML = iconQuestion;
+                    } else {
+                        tableExpandItemIcon.classList.add('danger');
+                        tableExpandItemIcon.innerHTML = iconDecl;
+                    }
+                    tableExpandItemAction.innerHTML = actions.join('');
+
+                    tableExpandItemDocument.appendChild(tableExpandItemIcon);
+                    tableExpandItemDocument.appendChild(tableExpandItemText);
+                    tableExpandItem.appendChild(tableExpandItemDocument);
+                    tableExpandItem.appendChild(tableExpandItemAction);
+                    tableExpand.appendChild(tableExpandItem);
+                    td.appendChild(tableExpand);
+                    tr.appendChild(td);
                 }
-                tableExpandItemAction.innerHTML = actions.join('');
 
-                tableExpandItemDocument.appendChild(tableExpandItemIcon);
-                tableExpandItemDocument.appendChild(tableExpandItemText);
-                tableExpandItem.appendChild(tableExpandItemDocument);
-                tableExpandItem.appendChild(tableExpandItemAction);
-                tableExpand.appendChild(tableExpandItem);
-                td.appendChild(tableExpand);
-                tr.appendChild(td);
                 tableBody.appendChild(tr);
             }
             let rows = tableBody.querySelectorAll('tbody tr');
@@ -205,10 +243,15 @@ include VIEWS . 'component/btn-icon.php';
                 programStudi: 'Teknik Informatika',
                 status: 'Tertanggung'
             },
-            data_detail: {
-                dokumen: 'test',
-                status: 'diverifikasi'
-            }
+            data_detail: [{
+                    dokumen: 'test',
+                    status: 'diverifikasi'
+                },
+                {
+                    dokumen: 'waaa',
+                    status: 'diverifikasi'
+                }
+            ]
         }, {
             data_mahasiswa: {
                 id: 1,
@@ -218,34 +261,49 @@ include VIEWS . 'component/btn-icon.php';
                 programStudi: 'Teknik Informatika',
                 status: 'Tertanggung'
             },
-            data_detail: {
+            data_detail: [{
                 dokumen: 'test',
                 status: 'Tertanggung'
-            }
+            },{
+                dokumen: 'test2',
+                status: 'Tertanggung'
+            }]
         }];
 
         document.getElementById('table-body').append(...generateTableBodyItems(test).children);
 
         document.getElementById('dialog-acc').addEventListener('submit', function(e) {
             e.preventDefault();
-            fetch(window.location.href, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        acc: this.acc.checked
-                    })
-                })
-                .then(response => response.json())
-                .then(data => {
-                    if (data.status === 'success') {
-                        window.location.reload();
-                    } else {
-                        alert(data.message);
-                    }
-                })
-                .catch(error => console.error(error));
+            $.ajax({
+                type: "POST",
+                url: "",
+                data: {
+
+                },
+                success: function() {
+
+                },
+                error: function(response) {
+
+                }
+            });
+        });
+
+        document.getElementById('dialog-acc').addEventListener('submit', function(e) {
+            e.preventDefault();
+            $.ajax({
+                type: "POST",
+                url: "",
+                data: {
+
+                },
+                success: function() {
+
+                },
+                error: function(response) {
+
+                }
+            });
         });
     </script>
 
--- app/views/component/btn-icon.php
+++ app/views/component/btn-icon.php
@@ -1,8 +1,8 @@
 <?php
 
-function iconButton($id, $icon, $color = 'black')
+function iconButton($id, $icon, $color = 'black', $onclick = '')
 {
-    echo '<button class="btn btn-icon" type="button" ' . ($id !== '' ? 'data-bs-toggle="modal" data-bs-target="#' . $id . '"' : '') . '>';
+    echo '<button class="btn btn-icon" type="button" ' . ($id !== '' ? 'data-bs-toggle="modal" data-bs-target="#' . $id . '"' : '') . ' onclick="' . $onclick . '">';
     echo SvgIcons::getIconWithColor($icon, $color);
     echo '</button>';
 }
