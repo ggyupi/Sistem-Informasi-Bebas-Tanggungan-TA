2d51eec29af4eb647c094518667d5fbfd53f3e03
ren5602 <naziz5602@gmail.com>
2024-12-01 20:49:52 +0700

    display komentar ke user,

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -99,6 +99,7 @@ class AdminController extends Controller
                 $_POST['nim'],
                 $this->admin->getPeopleId(),
                 $_POST['acc'] === 'true' ? StatusDokumen::Diverifikasi : StatusDokumen::Ditolak,
+                // $_POST['acc'] === 'true' ? null :
                 isset($_POST['komentar']) ? $_POST['komentar'] : ''
             );
         }
