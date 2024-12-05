0a3c6eb1725a1478d65809503160b585ef62bbdb
ren5602 <naziz5602@gmail.com>
2024-12-05 11:23:33 +0700

    notify raw

--- app/views/user/screens/notification.php
+++ app/views/user/screens/notification.php
@@ -1,21 +1,64 @@
+<?php
+include_once VIEWS . 'component/btn-icon.php';
+?>
 <style>
-    #notification-wrapper {
-        background-color: var(--bs-body-bg);
+    #notification-item {
         flex-direction: column;
         align-items: center;
         justify-content: center;
-        gap: 32px;
-        padding: 24px 50px;
+        gap: 16px;
+        padding: 24px 24px;
         border-radius: 12px;
         width: fit-content;
         height: fit-content;
+        margin: 16px;
+
+    }
+
+    /* bg item*/
+    #notification-content {
+        border-radius: 12px;
+        background-color: var(--bs-body-bg);
+        display: flex;
+        flex-direction: column;
+        align-items: start;
+        justify-content: start;
+        padding: 5px 5px;
+        border-radius: 12px;
+        width: fit-content;
+        margin: 8px;
+    }
+
+    .status-badge-text {
+        padding-top: 0px;
+    }
+
+    .status-badge-text svg {
+        height: 16px;
+        width: 16px;
     }
 </style>
-<div class="d-flex flex-column align-items-center justify-content-center"></div>
-<div class="d-flex flex-row" id="notification-wrapper">
-    <div class="status-badge-text danger"><?= SvgIcons::getIconWithColor(Icons::Close, "white") ?>
+<div class="d-flex flex-column align-items-start justify-content-center" id="notification-wrapper">
+    <div id="notification-content">
+        <div class="d-flex flex-row align-items-center justify-content-center danger-bg" id="notification-item">
+            <div class="status-badge-text danger"><?= SvgIcons::getIconWithColor(Icons::Close, "white") ?></div>
+            <div class="d-flex">
+                <div id="notification-document">
+                    Dokumen ditolak, silahkan perbaiki dokumen .....
+                </div>
+            </div>
+            <?= iconButton('', Icons::OpenInNewTab, 'var(--bs-emphasis-color)') ?>
+        </div>
     </div>
-    <div class="d-flex" style="gap: 16px;">
-        Dokumen ditolak, silahkan perbaiki dokumen .....
+    <div id="notification-content">
+        <div class="d-flex flex-row align-items-center justify-content-center danger-bg" id="notification-item">
+            <div class="status-badge-text danger"><?= SvgIcons::getIconWithColor(Icons::Close, "white") ?></div>
+            <div class="d-flex">
+                <div id="notification-document">
+                    Dokumen ditolak, silahkan perbaiki dokumen .....
+                </div>
+            </div>
+            <?= iconButton('', Icons::OpenInNewTab, 'var(--bs-emphasis-color)') ?>
+        </div>
     </div>
 </div>
